71a6c18e1d9ca5dc69260c340ec1f3f345238d39
rahafbateelz <rahafmiqdad@bateelz.com>
2022-02-08 20:38:42 +0200

    reg login profile

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -44,7 +44,6 @@
                       <option value="Albania">Albania</option>
                       <option value="Algeria">Algeria</option>
                       <option value="American Samoa">American Samoa</option>
-                      <option value="Andorra">Andorra</option>
                       <option value="Angola">Angola</option>
                       <option value="Anguilla">Anguilla</option>
                       <option value="Antigua &amp; Barbuda">Antigua &amp; Barbuda</option>
